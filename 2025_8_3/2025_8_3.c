a017a3c7a5cba5f152725faafd4204c852cb85b2
程中原 <1651333184@qq.com>
2025-08-04 17:34:35 +0800

    单链表

--- 2025_8_3/2025_8_3.c
+++ 2025_8_3/2025_8_3.c
@@ -1,18 +1,20 @@
-#include"SeqList.h"
-
-void TestSeqList1()
-{
-    SL s;
-    SeqListInit(&s);
-    SeqListPushBack(&s, 1);
-    SeqListPushBack(&s, 2);
-    SeqListPushBack(&s, 3);
-    SeqListInsert(&s, 1, 4);
-}
+#include"SList.h"
 
 //测试头尾插入删除
 int main()
 {
-    TestSeqList1();
-    printf("TestSeqList1 passed\n");
+    SListNode* Node = NULL;
+    //测试尾插法
+    // 这里传进去的就是指针的值
+    SListPushBack(&Node, 1);
+    SListPushBack(&Node, 2);
+    SListPushBack(&Node, 3);
+    SListPrint(Node);
+    SListPopBack(&Node);
+    SListPrint(Node);
+    SListPopBack(&Node);
+    SListPrint(Node);
+    SListPopBack(&Node);
+    SListPrint(Node);
+    printf("NULL\n");
 }
