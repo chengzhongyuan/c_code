1b9fa3e53f291ae03497a8dc3458dae8516b2abd
程中原 <1651333184@qq.com>
2025-08-31 10:21:46 +0800

    栈

---
+++ 2025_8_27/test.c
@@ -0,0 +1,20 @@
+#include "stack_queue.h"
+int main() {
+    Stack st;
+    StackInit(&st);
+
+    StackPush(&st, 10);
+    StackPush(&st, 20);
+    StackPush(&st, 30);
+
+    while (!StackEmpty(&st)) {
+        printf("Top: %d\n", StackTop(&st));
+        StackPop(&st);
+    }
+
+    StackDestroy(&st);
+    return 0;
+}
+
+
+// 在迷宫问题中后进先出，原路退回
