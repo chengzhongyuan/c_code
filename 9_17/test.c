d820303c97fbe2c0e768d67daa55beab2b7b5fc6
程中原 <1651333184@qq.com>
2025-09-17 14:53:47 +0800

    增加叶子节点

--- 9_17/test.c
+++ 9_17/test.c
@@ -54,6 +54,18 @@ int TreeSize(TNode* root)
     if (root == NULL)
         return 0;
     return 1 + TreeSize(root->left) + TreeSize(root->right);
+    // 其实所有的数据增加都是靠这个1走到根部之后都是0
+    
+}
+
+// 如果我只想要叶子节点的个数呢
+int LeafCount(TNode* root)
+{
+    if (root == NULL)
+        return 0;
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+    return LeafCount(root->left) + LeafCount(root->right);
 }
 
 TNode* newNode(int v) {
